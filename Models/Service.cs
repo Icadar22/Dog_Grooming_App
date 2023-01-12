057ba8fcaa17465540c2842b2c097d11b2669961
Icadar22 <cadariuliana2000@yahoo.com>
2023-01-12 20:26:02 +0200

    Update

--- Models/Service.cs
+++ Models/Service.cs
@@ -9,7 +9,6 @@ namespace Dog_Grooming_App.Models
     public class Service
     {
         public string Name { get; set; }
-        public string Description { get; set; }
         public ImageSource Image { get; set; }
         public string Details { get; set; }
         public double Price { get; set; }
